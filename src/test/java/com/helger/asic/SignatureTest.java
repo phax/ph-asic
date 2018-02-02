4e56fa642642c94135d8af87a540841092b0124f
Philip Helger <philip@helger.com>
2018-02-02 16:50:56 +0100

    Updated license headers

--- src/test/java/com/helger/asic/SignatureTest.java
+++ src/test/java/com/helger/asic/SignatureTest.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) 2015-2017 difi (www.difi.no)
+ * Copyright (C) 2018 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed
+ * with this file, You can obtain one at
+ * https://mozilla.org/MPL/2.0/
+ */
 package com.helger.asic;
 
 import static org.junit.Assert.assertNotNull;
