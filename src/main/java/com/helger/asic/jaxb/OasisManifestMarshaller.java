46874ed95b692621a61e15655f8d651a565b8cac
Philip Helger <philip@helger.com>
2023-04-22 13:19:01 +0200

    Added license header

--- src/main/java/com/helger/asic/jaxb/OasisManifestMarshaller.java
+++ src/main/java/com/helger/asic/jaxb/OasisManifestMarshaller.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2015-2017 difi (www.difi.no)
+ * Copyright (C) 2018-2023 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed
+ * with this file, You can obtain one at
+ * https://mozilla.org/MPL/2.0/
+ */
 package com.helger.asic.jaxb;
 
 import java.util.List;
@@ -13,7 +24,7 @@ import com.helger.xml.namespace.MapBasedNamespaceContext;
 import com.helger.xsds.xmldsig.CXMLDSig;
 
 /**
- * JAXB marshaller for the {@link ManifestType}.
+ * JAXB marshaller for the {@link Manifest}.
  *
  * @author Philip Helger
  * @since 3.0.0
